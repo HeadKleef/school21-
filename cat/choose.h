d1b474153cd189d1397c66f6d472af3762f7e62a
HeadKleef <138529488+HeadKleef@users.noreply.github.com>
2023-10-19 17:05:51 +0300

    Update choose.h

--- cat/choose.h
+++ cat/choose.h
@@ -4,7 +4,7 @@
 #ifndef CHOOSE_H_
 #define CHOOSE_H_
 
-void choose_flag(int p, char ch, FILE *file);
+int choose_flag(int i, int p, char ch, FILE *file);
 int is_empty_line(FILE *file);
 int new_line(char c);
 int is_tab(char c);

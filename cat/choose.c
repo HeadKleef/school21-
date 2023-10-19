4b2aa051f71ccaecc310d3353143dbb1b5325bb8
HeadKleef <138529488+HeadKleef@users.noreply.github.com>
2023-10-19 17:44:28 +0300

    Update choose.c

--- cat/choose.c
+++ cat/choose.c
@@ -14,16 +14,14 @@ int is_empty_line(FILE *file) {
   ungetc(d, file);
   return empty;
 }
-
-int choose_flag(int i,int p, char ch, FILE *file) {
-  if (p == 1) {
-    
-    if (ch == '\n' || ch == 4)
+void option_e(char ch){
+ if (ch == '\n' || ch == 4)
       printf("$\n");
     else
       putchar(ch);
-  } else if (p == 2) {
-    if (i == 1) {
+}
+int option_n(int i, char ch){
+if (i == 1) {
       printf(" %d ", i);
       i++;
     } else if (ch == '\n') {
@@ -31,8 +29,10 @@ int choose_flag(int i,int p, char ch, FILE *file) {
       i++;
     } else
       putchar(ch);
-  } else if (p == 3) {
-    if (i == 1) {
+  return i;
+}
+int option_b(int i, char ch, FILE *file){
+ if (i == 1) {
       printf(" %d ", i);
       i++;
     } else if (ch == '\n' && !is_empty_line(file)) {
@@ -40,18 +40,33 @@ int choose_flag(int i,int p, char ch, FILE *file) {
       i++;
     } else
       putchar(ch);
-  } else if (p == 4) {
-    if (is_empty_line(file) && new_line(ch)) {
+  return i;
+}
+void option_s(char ch, FILE*file){
+  if (is_empty_line(file) && new_line(ch)) {
       if (ch == EOF) {
         ungetc(ch, file);
       }
     } else
       putchar(ch);
-  } else if (p == 5) {
-    if (is_tab(ch)) {
+}
+void option_t(char ch){
+  if (is_tab(ch)) {
       printf("^|");
     } else
       putchar(ch);
+}
+int choose_flag(int i,int p, char ch, FILE *file) {
+  if (p == 1) {
+    option_e(ch);
+  } else if (p == 2) {
+    i = option_n(i, ch);
+  } else if (p == 3) {
+    i = option_b(i, ch, file);
+  } else if (p == 4) {
+    option_s(ch, file);
+  } else if (p == 5) {
+    option_t(ch);
   }
   return i;
 }

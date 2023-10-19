fa5d76cebef7c6bc6c072c405e137d7577133215
HeadKleef <138529488+HeadKleef@users.noreply.github.com>
2023-10-19 17:44:42 +0300

    Update choose.h

--- cat/choose.h
+++ cat/choose.h
@@ -8,5 +8,10 @@ int choose_flag(int i, int p, char ch, FILE *file);
 int is_empty_line(FILE *file);
 int new_line(char c);
 int is_tab(char c);
+void option_e(char ch);
+int option_n(int i, char ch);
+int option_b(int i, char ch, FILE *file);
+void option_s(char ch, FILE *file);
+void option_t(char ch);
 
 #endif
